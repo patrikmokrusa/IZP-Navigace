a753a3cf0c3301e2da259e669856a1ca6d9c25ba
Patrik <xmokrup00@vutbr.cz>
2023-10-06 18:48:39 +0200

    pridani funkce countarr

--- keyfilter.c
+++ keyfilter.c
@@ -2,8 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int countarr(char ar[42][100]){     //funkce pro ziskani poctu "adres" z pole
+    int x=0;
+    int element;
+    do {
+        element = ar[x][0];
+        x = x+1;
+
+
+    }while (element !=0);
+
+    return x-1;
+
+}
+
+
 int main()
 {
 
+  int len = countarr;
+ 
+  
   return EXIT_SUCCESS;
 }
