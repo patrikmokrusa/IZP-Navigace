390d847bbe496dc6454ff27de6da0403b2ce5bb9
Patrik <xmokrup00@vutbr.cz>
2023-10-16 18:36:05 +0200

    chybi jenom abecedne seradit + oddelat duplicates

--- keyfilter.c
+++ keyfilter.c
@@ -4,114 +4,97 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 
 
 int main(int argc,char *argv[])
 {
-  if (argc ==1){
-    printf("nezadany parametr\n");
+  if (argc <2){                           //if no parameter
+    printf("No parameter\n");
     return 1;
   }
 
-  char txt_file[42][100];
+
+  char txt_file[42][101];                 //declare list 42 lines, max 100 chars per line (+ '\0')
   int len = 0;
 
-  for (int i =0; i < 42; i++){     //nacist <adresy.txt
+  for (int i =0; i < 42; i++){                //input stdin
     
-    if (scanf("%s\n",txt_file[i]) == EOF){
+    if (scanf("%s\n",txt_file[i]) == EOF){             //if EOF stop inputting
       break;
     }
-    // printf("%s\n",txt_file[i]);
     len++;
   }
 
   
-  for (int i = 0; i <len; i++){
+  for (int i = 0; i <len; i++){                         //convert list to uppercase
     for (size_t y =0; y < strlen(txt_file[i]); y++)
     {
       txt_file[i][y]= toupper(txt_file[i][y]);
-      // printf("%c",txt_file[i][y]);
     }
-    printf("\n");
   }
   
-  size_t argcount = strlen(argv[1]);
-  for (size_t i = 0; i < argcount ; i++)
+  size_t len_arg = strlen(argv[1]);             //convert argument to uppercase
+  for (size_t i = 0; i < len_arg ; i++)
   {
     argv[1][i]= toupper(argv[1][i]);
   }
 
-  // for (int i = 0; i < len; i++)     //zjistit jestli tam adrese je primo
-  // {
-  //   if (argv[1] == txt_file[i])
-  //   {
-  //     printf("Found: %s",txt_file[i]);
-  //     // return 0;
-  //   }
-    
-  // }
-  
 
   char *filtered[42];
   size_t num = 0;
-  int num_filtr =0;
+  int len_filter =0;
   int count = 0;
 
-  for (int i = 0; i < len; i++)
+  for (int i = 0; i < len; i++)                //if "address" matches argument add it to filtered
   {
-    for (size_t x = 0; x < argcount; x++)
+    for (size_t x = 0; x < len_arg; x++)
     {
       if (txt_file[i][x] == argv[1][x])
       {
         num++;
       }
-      
     }
-    if (num == argcount){
-      filtered[num_filtr] = txt_file[i];
-      // printf("filtered: %s\n",filtered[num_filtr]);
-      num_filtr++;
+    if (num == len_arg){
+      filtered[len_filter] = txt_file[i];
+      len_filter++;
       count++;
     }
     num = 0;
   }
-  
 
-  // char filtered[42];                                                              //chelp
-  // for (size_t z = 0; z<argcount; z++){ // pocet argumentu
-  //   int num = 0;
-  //   for (int i = 0; i < len; i++) // pocet radku v txt
-  //   {
-  //     if (txt_file[i][z] == argv[1][z]) // porovnani s argumentem
-  //     {
-  //       filtered[num] = txt_file[i];
-  //       // printf("filter: %s\n",filtered[num]);
-  //       num++;
-  //     }
-  //   }
-  //   int count = 0;
-  //   while (txt_file[count][0] !=  '\0')
-  //   {
-  //     txt_file[count][0] = filtered[count];
-  //     count++;
-  //     // printf("plsky %s\n",txt_file[count]);
-  //   }
-  // }
   
-  
-
-  printf("Enable: ");
-  for (int i = 0; i < count; i++)
+  int found = false;
+  if (found == false)                         //check if full argument in filtered
   {
-    printf("%c",filtered[i][argcount]);
+      for (int i = 0; i < len_filter; i++)
+    {
+      if ((argv[1][len_arg] == '\0') && (filtered[i][len_arg] =='\0'))
+      {
+        printf("Found: %s",filtered[i]);
+        found = true;
+      }
+    }
+  }
+  if (len_filter == 0)                 //if nouting filtered
+  {
+    printf("Not Found");  
+  }
+  else if(found == false)              //print the next char after argument
+  {
+    printf("Enable: ");                    
+    for (int i = 0; i < count; i++)
+    {
+      printf("%c",filtered[i][len_arg]);
+    }  
   }
   
-  // printf("%d\n",argcount);
+
+  // printf("%d\n",len_arg);
   // printf("\nargumenty %s\n",argv[1]);
   // printf("\nPocet mest: %d\n",len);
-
   // printf("%c\n",*filtered[1]);
   printf("\n");
-  return EXIT_SUCCESS;
+  return 0;
 }
